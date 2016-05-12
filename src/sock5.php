b5f0ee454be657136d50ff46d76710f90b04287e
Iceux <liubinzh@gmail.com>
2016-05-12 13:53:24 +0800

    bug fix

--- src/sock5.php
+++ src/sock5.php
@@ -19,7 +19,8 @@ define('ADDRTYPE_IPV4', 1);
 define('ADDRTYPE_IPV6', 4);
 define('ADDRTYPE_HOST', 3);
 
-class Sock5Server{
+class Sock5Server
+{
 	protected $serv = array();
 	// 前端
 	protected $frontends;
@@ -30,44 +31,49 @@ class Sock5Server{
 	// config
 	protected $config;
 
-	public function __construct(){
-		$this->config =  array('daemon'=>false, 'host'=>'0.0.0.0', 'port'=>1080);
+	public function __construct()
+	{
+		$this->config = array('daemon' => false, 'host' => '0.0.0.0', 'port' => 1080);
 		$argv = getopt('c:d');
-		if(isset($argv['d'])){
+		if (isset($argv['d'])) {
 			$this->config['daemon'] = true;
 		}
 		$config = empty($argv['c']) ? '' : getcwd() . '/' . $argv['c'];
-		if($config){
-			if (!file_exists($config)){
+		if ($config) {
+			if (!file_exists($config)) {
 				throw new \Exception('config file is not exists');
 			}
 			$config = file_get_contents($config);
-			if($config){
+			if ($config) {
 				$config = json_decode($config, true);
 			}
 		}
-		if(is_array($config)){
+		if (is_array($config)) {
 			$this->config = array_merge($this->config, $config);
 		}
 		$this->serv = new \swoole_server($this->config['host'], $this->config['port'],
 			SWOOLE_PROCESS, SWOOLE_SOCK_TCP);
-		$this->serv->on('connect', [$this,'onConnect']);
-		$this->serv->on('receive', [$this,'onReceive']);
-		$this->serv->on('close', [$this,'onClose']);
-		$this->logger = new \Katzgrau\KLogger\Logger(getcwd().'/logs');
+		$this->serv->on('connect', [$this, 'onConnect']);
+		$this->serv->on('receive', [$this, 'onReceive']);
+		$this->serv->on('close', [$this, 'onClose']);
+		$this->logger = new \Katzgrau\KLogger\Logger(getcwd() . '/logs');
 	}
-	public function onConnect($serv, $fd){
+
+	public function onConnect($serv, $fd)
+	{
 		// 设置当前连接的状态为STAGE_INIT，初始状态
-		if(!isset($this->frontends[$fd])){
+		if (!isset($this->frontends[$fd])) {
 			$this->frontends[$fd]['stage'] = STAGE_INIT;
 		}
 	}
-	public function onReceive($serv, $fd, $from_id, $data){
+
+	public function onReceive($serv, $fd, $from_id, $data)
+	{
 		$connection = isset($this->frontends[$fd]) ? $this->frontends[$fd] : false;
-		if(!$connection){
+		if (!$connection) {
 			$this->frontends[$fd]['stage'] = STAGE_INIT;
 		}
-		switch ($this->frontends[$fd]['stage']){
+		switch ($this->frontends[$fd]['stage']) {
 			case STAGE_INIT:
 				//与客户端建立SOCKS5连接
 				//参见: https://www.ietf.org/rfc/rfc1928.txt
@@ -77,72 +83,75 @@ class Sock5Server{
 			case STAGE_ADDR:
 				$cmd = ord($data[1]);
 				//仅处理客户端的TCP连接请求
-				if($cmd != CMD_CONNECT){
+				if ($cmd != CMD_CONNECT) {
 					$this->logger->error('unsupport cmd');
 					$serv->send($fd, "\x05\x07\x00\x01");
 					return $this->serv->close($fd);
 				}
 				$header = $this->parse_socket5_header($data);
-				if(!$header){
+				if (!$header) {
 					$serv->send($fd, "\x05\x08\x00\x01");
 					return $this->serv->close($fd);
 				}
 				//尚未建立连接
-				if (!isset($this->frontends[$fd]['socket'])){
+				if (!isset($this->frontends[$fd]['socket'])) {
 					$this->frontends[$fd]['stage'] = STAGE_CONNECTING;
 					//连接到后台服务器
 					$socket = new \swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
 					$socket->closing = false;
-					$socket->on('connect', function (\swoole_client $socket) use ($data, $fd){
+					$socket->on('connect', function (\swoole_client $socket) use ($data, $fd) {
 						$this->logger->debug("onConnect: backend[{$socket->sock}]");
 						$this->backends[$socket->sock] = $fd;
 						$this->frontends[$fd]['socket'] = $socket;
 						$this->frontends[$fd]['stage'] = STAGE_STREAM;
-						$buf_replies = "\x05\x00\x00\x01\x00\x00\x00\x00". pack('n', 8888);
+						$buf_replies = "\x05\x00\x00\x01\x00\x00\x00\x00" . pack('n', 8888);
 						$this->serv->send($fd, $buf_replies);
 					});
-					$socket->on('error', function (\swoole_client $socket) use ($fd){
+					$socket->on('error', function (\swoole_client $socket) use ($fd) {
 						$this->logger->error("connect to backend server failed");
 						$this->serv->send($fd, "backend server not connected. please try reconnect.");
 						$this->serv->close($fd);
 					});
-					$socket->on('close', function (\swoole_client $socket) use ($fd){
+					$socket->on('close', function (\swoole_client $socket) use ($fd) {
 						unset($this->backends[$socket->sock]);
 						unset($this->frontends[$fd]);
-						if (!$socket->closing){
+						if (!$socket->closing) {
 							$this->serv->close($fd);
 						}
 					});
-					$socket->on('receive', function (\swoole_client $socket, $_data) use ($fd){
+					$socket->on('receive', function (\swoole_client $socket, $_data) use ($fd) {
 						$this->serv->send($fd, $_data);
 					});
 
-					if($header[0] == ADDRTYPE_HOST){
-						\swoole_async_dns_lookup($header[1], function ($host, $ip) use($header, $socket, $fd){
+					if ($header[0] == ADDRTYPE_HOST) {
+						\swoole_async_dns_lookup($header[1], function ($host, $ip) use ($header, $socket, $fd) {
 							$connection_info = $this->serv->connection_info($fd);
 							$this->logger->info("connecting {$host}:{$header[2]} from {$connection_info['remote_ip']}:{$connection_info['remote_port']}");
 							$socket->connect($ip, $header[2]);
 						});
-					}elseif($header[0] == ADDRTYPE_IPV4){
+					} elseif ($header[0] == ADDRTYPE_IPV4) {
 						$socket->connect($header[1], $header[2]);
-					}else{
+					} else {
 						$serv->send($fd, "\x05\x08\x00\x01");
 						return $this->serv->close($fd);
 					}
 				}
 				break;
 			case STAGE_STREAM:
-				if (isset($this->frontends[$fd]['socket'])){
+				if (isset($this->frontends[$fd]['socket'])) {
 					$socket = $this->frontends[$fd]['socket'];
 					$socket->send($data);
 				}
 				break;
-			default:break;
+			default:
+				break;
 		}
 	}
-	public function onClose($serv, $fd, $from_id){
+
+	public function onClose($serv, $fd, $from_id)
+	{
 		//清理掉后端连接
-		if (isset($this->frontends[$fd]['socket'])){
+		if (isset($this->frontends[$fd]['socket'])) {
 			$backend_socket = $this->frontends[$fd]['socket'];
 			$backend_socket->closing = true;
 			$backend_socket->close();
@@ -151,7 +160,9 @@ class Sock5Server{
 		}
 		$this->logger->debug("onClose: frontend[$fd]");
 	}
-	public function start(){
+
+	public function start()
+	{
 		$default = [
 			'daemonize' => $this->config['daemon'],
 			'timeout' => 1,
@@ -164,16 +175,18 @@ class Sock5Server{
 		$this->serv->set($default);
 		$this->serv->start();
 	}
+
 	/**
 	 * 解析客户端发来的socket5头部数据
 	 * @param string $buffer
 	 */
-	protected function parse_socket5_header($buffer){
+	protected function parse_socket5_header($buffer)
+	{
 		$buffer = substr($buffer, 3);
 		$addr_type = ord($buffer[0]);
-		switch($addr_type){
+		switch ($addr_type) {
 			case ADDRTYPE_IPV4:
-				$dest_addr = ord($buffer[1]).'.'.ord($buffer[2]).'.'.ord($buffer[3]).'.'.ord($buffer[4]);
+				$dest_addr = ord($buffer[1]) . '.' . ord($buffer[2]) . '.' . ord($buffer[3]) . '.' . ord($buffer[4]);
 				$port_data = unpack('n', substr($buffer, 5, 2));
 				$dest_port = $port_data[1];
 				$header_length = 7;
